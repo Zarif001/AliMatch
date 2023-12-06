0a759b015e0903502ac67dd663056d6e489f84d6
zarif <zarif@icloud.com>
2023-12-06 10:57:34 +0500

    test commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import styles from './Contact.module.scss'
-
+import { useState } from 'react'
+import Submit from '../Submit/Submit'
 function Contact() {
+    const [modal, setModal] = useState(false)
+
+    const openModal = () =>{
+      setModal(true)
+    }
+    const closeModal = () =>{
+      setModal(false)
+    }
     return(
         <div className={styles.contact}>
             <>
             <div className={styles.box}>
             
-            <button>
+            <button onClick={openModal}>
             <img src="./images/callButton.svg" alt="callButton" />
                 Заказать обратный звонок эксперта</button>
             </div>
             </>
-         
+            {
+            modal && (
+              <Submit closeModal={closeModal}  />
+            )
+          }
         </div>
     )
 }
